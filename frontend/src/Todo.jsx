ff75422a4d2a648165de3979dbc188bf0c16cccf
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2024-01-18 16:19:56 +0000

    Fixed title key in Todo

--- frontend/src/Todo.jsx
+++ frontend/src/Todo.jsx
@@ -1,6 +1,6 @@
 export function Todo({todos ,setRefresh }) {
   function markAsDone(id , route){
-    fetch(`https://congenial-zebra-764qr55vwpf455-3000.app.github.dev/${route}` , {
+    fetch(`https://curly-halibut-5pvjppg6w7wc7j4q-3000.app.github.dev/${route}` , {
       method: "PUT",
       headers: {
         "Content-Type": "application/json"
@@ -13,7 +13,7 @@ export function Todo({todos ,setRefresh }) {
     <>
       {todos.map((todo) => {
         let completed = todo?.completed;
-        return (<div  className={`border-2 flex flex-col ${completed? "bg-green-50" : "bg-yellow-50"}`} key={"title"}>
+        return (<div  className={`border-2 flex flex-col ${completed? "bg-green-50" : "bg-yellow-50"}`} key={todo["_id"]}>
         <h1 className='text-3xl'>{todo["title"]}</h1>
         <h1>{todo.description}</h1>
         <button onClick={() => {markAsDone(todo._id , "completed")}} className={`${completed ? "bg-green-300" : "bg-yellow-300"} border-2 ml-auto`} id={todo?._id}>{ completed ? "Completed" : "Mark as done"}</button>
