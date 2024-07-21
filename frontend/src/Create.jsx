cd57c73e9fac3d7171e8e9b528df97724b504159
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2024-07-22 02:28:09 +0530

    Add Input label component

--- frontend/src/Create.jsx
+++ frontend/src/Create.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react'
 import { Input } from "@/components/ui/input"
 import { Button } from './components/ui/button'
+import { FloatingInput, FloatingLabel, FloatingLabelInput } from './components/ui/floating-label-input'
 export function Create({ setRefresh }) {
     let title = useRef(null)
     let des = useRef(null)
@@ -18,8 +19,16 @@ export function Create({ setRefresh }) {
     return (
         <>
             <div className='flex flex-col  justify-center border-2 '>
-                <div className='mx-auto p-2'><Input className='bg-white border-2 mx-2 ' type="text" ref={title} placeholder="Title"/></div>
-                <div className='mx-auto p-2'><Input className='bg-white border-2 mx-2' type="text" ref={des} placeholder="Descritption"/></div>
+                <div className='mt-10 relative mx-auto'>
+                    <FloatingInput id="floating-customize" className='bg-white mx-2' type="text" ref={title} />
+                    <FloatingLabel htmlFor="floating-customize">Title</FloatingLabel>
+                </div>
+                {/* <div className='mx-auto p-2'><FloatingLabelInput className='bg-white border-2 mx-2' type="text" ref={title} label="Title" /></div> */}
+                <div className='relative mx-auto mt-4'>
+                    <FloatingInput id="floating-customize1" className='bg-white mx-2' type="text" ref={des} />
+                    <FloatingLabel htmlFor="floating-customize1">Description</FloatingLabel>
+                    {/* <FloatingLabelInput className='bg-white border-2 mx-2' type="text" ref={des} label="Description" /> */}
+                </div>
                 <br />
                 <Button className='mx-auto' onClick={() => { title.current.value && des.current.value ? create(title.current.value, des.current.value) : alert("Invalid input"); title.current.value = ""; des.current.value = ""; }} >Create</Button>
                 <br />
